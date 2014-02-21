4f7a30c7f8494356676364754936cc48baf2785d
Hugo <hugo.viricel@gmail.com>
2014-02-21 20:07:56 +0100

    create form add event

--- plugins/partagedefrais/nouvel-evenement.php
+++ plugins/partagedefrais/nouvel-evenement.php
@@ -7,7 +7,28 @@
 
             <div id="o-main" class="span9">
 
-                <?php echo "test hugo"; ?>
+                <div id="updateform-<?php the_ID(); ?>" class="collapse">
+                    <?php
+                    echo get_the_ID();
+                    $args = array(
+                        'post_id' => get_the_ID(), // post id to get field groups from and save data to
+                        'field_groups' => array(31), // this will find the field groups for this post (post ID's of the acf post objects)
+                        'form' => true, // set this to false to prevent the <form> tag from being created
+                        'form_attributes' => array( // attributes will be added to the form element
+                            'id' => 'post',
+                            'class' => '',
+                            'action' => get_permalink( get_the_ID() ),
+                            'method' => 'post',
+                        ),
+                        'return' => add_query_arg( 'updated', 'true', get_permalink() ), // return url
+                        'html_before_fields' => '', // html inside form before fields
+                        'html_after_fields' => '', // html inside form after fields
+                        'submit_value' => 'Update', // value for submit field
+                        'updated_message' => 'Post updated.', // default updated message. Can be false to show no message
+                    );
+                    acf_form( $args );
+                    ?>
+                </div>
 
             </div><!-- /#o-main -->
 
--- themes/origines-child-master/functions.php
+++ themes/origines-child-master/functions.php
@@ -34,15 +34,46 @@ add_action('wp_enqueue_scripts', 'myorigines_styles');
  * @param int $post_id the newly created post id
  */
 function wpufe_add_user_participants( $post_id ) {
-	echo '<div id="message" class="error"><p>' . $post_id . '</p></div>';
-	$meta_values = get_post_meta( $post_id );
-	
-	echo '<div id="message" class="error"><p>';
-	var_dump($meta_values);
-	echo '</p></div>';
-	
-	return true;
-}
-add_filter( 'wpuf_add_post_after_insert', 'wpufe_add_user_participants' );
 
-?>
+	$meta_values = get_post_meta( $post_id );
+
+    if ( email_exists($user_email) == false ) {
+        $random_password = wp_generate_password( $length=12, $include_standard_special_chars=false );
+        $user_id = wp_create_user( $user_name, $random_password, $user_email );
+    } else {
+        $random_password = __('User already exists.  Password inherited.');
+    }
+
+}
+//add_filter( 'wpuf_add_post_after_insert', 'wpufe_add_user_participants' );
+
+
+function add_new_post( $post_id )
+{
+    if( $post_id == 'new' ) {
+        // Create a new post
+        $post = array(
+            'post_title' => $_POST["fields"]['field_52c810cb44c7a'],
+            'post_category' => array(4),
+            'post_status'  => 'draft',
+            'post_type'  => 'post'
+        );
+
+        // insert the post
+        $post_id = wp_insert_post( $post );
+
+        return $post_id;
+    }
+    else {
+        $post = array(
+            'ID' => $post_id,
+            'post_title' => $_POST["fields"]['field_52c810cb44c7a']
+        );
+
+        // update the post
+        $post_id = wp_update_post( $post );
+
+        return $post_id;
+        return $post_id;
+    }
+}add_filter('acf/pre_save_post' , 'add_new_post' );
