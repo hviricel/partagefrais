e3c7756f79a99617f5a80439b9aeff36c87e9be1
Hugo <hugo.viricel@gmail.com>
2014-02-21 20:26:14 +0100

    change text

--- themes/origines-child-master/nouvel-evenement.php
+++ themes/origines-child-master/nouvel-evenement.php
@@ -11,7 +11,10 @@
  */
 ?>
 
-<?php get_header(); ?>
+<?php
+    acf_form_head();
+    get_header();
+?>
 
     <div id="o-wrapper" class="container">
 
@@ -19,9 +22,8 @@
 
             <div id="o-main" class="span9">
 
-                <div id="updateform-<?php the_ID(); ?>" class="collapse">
+                <div id="create-event" >
                     <?php
-                    echo get_the_ID();
                     $args = array(
                         'post_id' => get_the_ID(), // post id to get field groups from and save data to
                         'field_groups' => array(31), // this will find the field groups for this post (post ID's of the acf post objects)
@@ -38,7 +40,26 @@
                         'submit_value' => 'Update', // value for submit field
                         'updated_message' => 'Post updated.', // default updated message. Can be false to show no message
                     );
-                    acf_form( $args );
+
+                    $options = array(
+                        'post_id' => false, // post id to get field groups from and save data to
+                        'field_groups' => array(), // this will find the field groups for this post (post ID's of the acf post objects)
+                        'form' => true, // set this to false to prevent the <form> tag from being created
+                        'form_attributes' => array( // attributes will be added to the form element
+                            'id' => 'post',
+                            'class' => '',
+                            'action' => '',
+                            'method' => 'post',
+                        ),
+                        'return' => add_query_arg( 'updated', 'true', get_permalink() ), // return url
+                        'html_before_fields' => '', // html inside form before fields
+                        'html_after_fields' => '', // html inside form after fields
+                        'submit_value' => 'Update', // value for submit field
+                        'updated_message' => 'Post updated.', // default updated message. Can be false to show no message
+                    );
+
+
+                    acf_form( $options );
                     ?>
                 </div>
 
